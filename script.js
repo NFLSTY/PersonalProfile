1d611f0a2261199443a48ab3d943225e1f72639b
NFLSTY <naufalsatya10@gmail.com>
2024-08-26 14:11:52 +0700

    simple complete PP

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function showAlert() {
 }
 
 document.getElementById("myButton").addEventListener("click", showAlert);
+document.getElementById("prj1").addEventListener("click", function() {
+    window.location.href = "https://github.com/NFLSTY/LibraryPlus.git";
+});
+
+document.getElementById("prj2").addEventListener("click", function() {
+    window.location.href = "https://github.com/NFLSTY/BookManagement.git";
+});
+
+document.getElementById("prj3").addEventListener("click", function() {
+    window.location.href = "https://github.com/NFLSTY/MoneySystem.git";
+});
